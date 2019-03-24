a50eaf4a333cabc98164006043aee9e35f0edd2e
Daniel Chi <danielhchi@gmail.com>
2019-03-24 01:31:07 -0400

    added materialize recipe card without supplementary css. added lots of buttons for future features. also added console log for api response in the javascript, go to slack for id and key.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,7 @@
-var recipe = ""
+//temporary ajax for dev purposes, go to SLACK for edamam id and keys
+$.ajax({
+    url: "https://api.edamam.com/search?q=chicken&app_id={SLACK}&app_key={SLACK}",
+    method: "GET"
+}).then(function(response){
+    console.log (response)
+})
